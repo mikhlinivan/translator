bc691c19fff9c52746af0b69c3bf522300e98090
mikhlinivan <SlenderNSK@gmail.com>
2018-06-17 10:44:05 +0700

    create word function

--- src/string.c
+++ src/string.c
@@ -1,1 +1,24 @@
 #include "stdio.h"
+#include "string.h"
+
+String *word(int size_of_word)
+{
+	String *arr;
+	arr = malloc(sizeof(String));
+	if(arr == NULL) {
+		return NULL;
+	}
+
+	if(size_of_word == 1) {
+		arr->array = malloc((size_of_word + 1)*sizeof(char));
+		arr->cap = size_of_word + 1;
+	}
+	else if(size_of_word > 1) {
+		arr->array = realloc((size_of_word + 1)*sizeof(char));
+		arr->cap = size_of_word + 1;
+	}
+	else {
+		return NULL;
+	}
+	return arr;
+}
--- src/string.h
+++ src/string.h
@@ -1,1 +1,9 @@
 #include "stdio.h"
+
+typedef struct {
+	int size;
+	int cap;
+	char *array;
+} String;
+
+String *word(int size_of_word);
